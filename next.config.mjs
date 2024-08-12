e2dcd28eeac806ffef9b9ca34ed69a66c8e027c0
dinnertime <dinnertime.dev@gmail.com>
2024-08-12 15:01:46 +0900

    cloudflare 배포 등록

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
@@ -10,5 +11,9 @@ const nextConfig = {
     },
   },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
