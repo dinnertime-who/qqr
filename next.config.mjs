57bd73f58bf04522cbc6fe79167eba339b875c74
devtor-who <dinnertime.dev@gmail.com>
2024-08-14 23:50:04 +0900

    이미지 최적화 형식 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
   experimental: {
